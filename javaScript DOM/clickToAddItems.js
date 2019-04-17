850f8eb6ccad9c7eadc8c66c7a76581257e3dd96
Trish <trishatolentino89@gmail.com>
2019-04-17 14:50:22 -0400

    Update clickToAddItems.js

--- javaScript DOM/clickToAddItems.js
+++ javaScript DOM/clickToAddItems.js
@@ -7,3 +7,29 @@ function clearErrors() {
     }
   }
 }
+
+function resetForm() {
+  clearErrors();
+  var num1 = document.forms["numberFun"]["num1"].value;
+  var num2 = document.forms["numberFun"]["num2"].value;
+  if (num1 == "" || isNaN(num1)) {
+    alert("Num1 must be filled in with a number.");
+    document.forms["numberFun"]["num1"].parentElement.className = "form-group-has-error";
+    document.forms["numberFun"]["num1"].focus();
+    return false;
+  }
+  if (num2 == "" || isNan(num2)) {
+    alert("Num2 must be filled in with a number.");
+    document.forms["numberFun"]["num2"].parentElement.className = "form-group-has-error";
+    document.forms["numberFun"]["num2"].focus();
+    return false;
+  }
+  document.getElementById("results").style.display = "block";
+  document.getElementById("submitButton").innerText = "Recalculate";
+  document.getElementById("addResult").innerText = Number(num1) +       Num(num2);
+  document.getElementById("subtractResult").innerText = num1 - num2;
+  document.getElementById("multiplyResult").innerText = num1 * num2;
+  document.getElementById("divideResult").innerText = num1 / num2;
+  //We are returning false so that the form doesn't submit and so that we can see the results
+  return false;
+}
