1ffcb4135c3f0d14409d62e4bc4a61632e3c89b4
ttolentino89 <trishatolentino89@gmail.com>
2019-04-23 09:18:56 -0400

    adding new variables to luckySevens

--- javaScript DOM/luckySevens.js
+++ javaScript DOM/luckySevens.js
@@ -15,17 +15,30 @@ function rollDice() {
   gameMoney = Number(gameMoney);
   // var gameMoney = placeBet()
   //will eventually need something like randomNumber = Math.floor(Math.random() * 6) to find dice1 and dice2 total;
-  var dice1 = return Math.floor(Math.random() * 6);
-  var dice2 = return Math.floor(Math.random() * 6);
+  var dice1;
+  var dice2;
   var diceTotal = dice1 + dice2;
+  var rollNumber;
+  var maxRolls;
+  var maxMoney = gameMoney - maxRolls
+  while (gameMoney > 0){
+    var dice1 = return Math.floor(Math.random() * 6);
+    var dice2 = return Math.floor(Math.random() * 6);
+    // var diceTotal = dice1 + dice2;
+    var rollNumber++
   }
   if (diceTotal = 7) {
     gameMoney = gameMoney + 4;
   } else {
-    gameMoney = gameMoney - 1;
+    gameMoney--;
   }
 return gameMoney
 }
+
+document.getElementById("starting-bet").innerText = startingBet;
+document.getElementById("max-rolls").innerText = howManyRolls;
+document.getElementById("max-money").innerText = maxMoney;
+document.getElementById("roll-count").innerText = highestRolls;
 
 // function youLose() {
 //   rollDice();
