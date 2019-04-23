f73fb7fd68aa5bb3763b059f3231ec56026b5e9d
Trish <trishatolentino89@gmail.com>
2019-04-23 15:00:35 -0400

    Update newPortfolio.js

--- newPortfolio.js
+++ newPortfolio.js
@@ -1,5 +1,7 @@
 function showMe() {
-    var x = document.getElementById("first-section");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+//     var x = document.getElementById("first-section");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+// }
+  document.getElementById("first-section").style.display = "block";
 }
